9496839df57dd154a05e02130764a4d19b1566a5
will95io <50805208+will95io@users.noreply.github.com>
2020-04-10 20:40:15 -0400

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function About () {
+  
+  return (
+    <div>
+      <h1>Welcome to our about page!</h1>
+      <br>
+      </br>
+      <h1>collaboration summary:</h1>
+      <p>TBD</p>
+    </div>
+  )
+}
+
+export default About;
