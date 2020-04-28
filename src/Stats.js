d7ffef1f032ab97d002a0825f99f076a6b966f22
will95io <50805208+will95io@users.noreply.github.com>
2020-04-28 16:19:45 -0400

    echo and update data

--- src/Stats.js
+++ src/Stats.js
@@ -1,16 +1,83 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
 import './App.css';
+import firebase from './firebase'
+import 'firebase/firestore';
 
-function Stats () {
-  
+function useReviews(){
+  const [reviews,setReviews] = useState([])
+
+  useEffect(()=>{
+     const unsubscribe = firebase
+     .firestore()
+     .collection('reviews')
+     .onSnapshot((snapshot)=>{
+        const newReviews = snapshot.docs.map((doc)=>({
+           id:doc.id,
+           ...doc.data()
+        }))
+
+        setReviews(newReviews)
+     })
+     return () => unsubscribe()
+  },[])
+  return reviews
+}
+
+const Stats = () => {
+
+      const reviews = useReviews()
+
   return (
     <div className="App">
       
-      <h1>Welcome to our stats page!</h1>
+      <h3>Welcome to our stats page!</h3>
    
-      <p>TBD</p>
+      <h3>Here are all our past reviews:</h3>
 
+      <div>
+        <label><p>Sort by:</p></label>{''}
+      <select>
+        <option>Location a-z</option>
+        <option>Name a-z</option>
+      </select>
     </div>
+
+
+    <div >
+    <ol className= "ol">
+      {reviews.map((name)=>
+      <li className="li" key= {name.id} >
+          Reviewer: {name.name} 
+          <br  />
+          Location: {name.location} 
+        <table border = '1px solid black' align = 'center'>
+        <thead>
+          <tr > 
+            <th align = 'center' >Bonus Features:</th> 
+            </tr>   
+            </thead>
+            <tbody>
+                <tr> 
+          {name.wifi? <td>Wifi &#10004;</td> : null }
+          {name.outlets? <td>Outlets &#10004;</td> : null }
+          {name.kid? <td>Kid Friendly &#10004;</td> :null}
+          {name.reward? <td>Reward Program &#10004;</td> : null}
+          {name.sofas? <td>Sofa(s) &#10004;</td> : null }
+          {name.order? <td> Order ahead &#10004;</td> : null }
+          {name.gluten? <td>Guten-Free Options&#10004;</td> : null }
+          {name.vegan? <td>Vegan Options &#10004;</td> : null}
+              </tr>
+              </tbody>
+          </table>
+        Review: {name.review} 
+      </li>
+    
+      )} 
+    </ol>
+    <br  />
+    </div>
+    </div>
+
   )
 }
 
